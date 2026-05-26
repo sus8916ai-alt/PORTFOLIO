39f871f439fcc9464ec5f2836d00b648431837a9
Sushil <sus8916sus8916.ai@gmail.com>
2026-05-26 14:42:45 +0530

    Portfolio using Angular

--- Portfolio/src/app/app.ts
+++ Portfolio/src/app/app.ts
@@ -1,12 +1,18 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, NgIf],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
-export class App {
-  protected readonly title = signal('Portfolio');
+export class App implements OnInit {
+  public readonly loaded = signal(false);
+
+  ngOnInit() {
+    setTimeout(() => this.loaded.set(true), 700);
+  }
 }
